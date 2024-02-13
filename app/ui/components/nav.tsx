37e2e81e7bfbe98b3d6b9f0250695a357d4c29c7
Long <hglong903@gmail.com>
2024-02-13 21:34:56 +0700

    add icon to nav

--- app/ui/components/nav.tsx
+++ app/ui/components/nav.tsx
@@ -4,11 +4,12 @@ import Link from "next/link";
 import { luckiest_guy } from "../fonts";
 import clsx from "clsx";
 import { usePathname } from "next/navigation";
+import { PlayIcon, UserCircleIcon, FireIcon } from "@heroicons/react/24/solid";
 
 const links = [
-  { name: "Profile", href: "/profile" },
-  { name: "Play", href: "/" },
-  { name: "Leaderboard", href: "/leaderboard" },
+  { name: "Profile", href: "/profile", icon: UserCircleIcon },
+  { name: "Play", href: "/", icon: PlayIcon },
+  { name: "Leaderboard", href: "/leaderboard", icon: FireIcon },
 ];
 
 export function Nav() {
@@ -17,17 +18,24 @@ export function Nav() {
     <div
       className={`${luckiest_guy.className} text-xl absolute bottom-0 w-full h-[42px] grid grid-cols-3 text-center bg-[#333] text-white`}
     >
-      {links.map((link) => (
-        <Link
-          key={link.name}
-          href={link.href}
-          className={clsx("text-2xl m-auto w-full rounded-sm", {
-            "bg-white text-black": pathName == link.href,
-          })}
-        >
-          {link.name}
-        </Link>
-      ))}
+      {links.map((link) => {
+        const LinkIcon = link.icon;
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={clsx(
+              "flex justify-center items-center text-2xl m-auto w-full rounded-sm",
+              {
+                "bg-white text-black": pathName == link.href,
+              }
+            )}
+          >
+            <LinkIcon className="h-6" />
+            <p className="hidden md:block">{link.name}</p>
+          </Link>
+        );
+      })}
     </div>
   );
 }
